b02ed9a05c874cf93912b5f609bb13e40de99e9e
sororne <sororddr@yahoo.co.jp>
2018-12-26 20:22:52 +0900

    turn to camera

--- LookAtController.cs
+++ LookAtController.cs
@@ -6,8 +6,12 @@ public class LookAtController : MonoBehaviour {
 
   Animator animator;
   Vector3 targetPos;
-  string path = @"/home/nagayoshi/openpose_ws/openpose/build/examples/tutorial_python/output.csv";
+  private string path = @"/home/nagayoshi/openpose_ws/openpose/build/examples/tutorial_python/output.csv";
   private float timeElapsed = 0.0f; // 経過時間
+  private float noChangeTime = 0.0f; // 座標が変化していない時間
+  private float last_x = 99.9f; // 直前の座標
+  private float last_y = 99.9f; // 直前の座標
+  private bool isChange = false; // 直前の座標から変化しているか
 
   void Start () {
     this.animator = GetComponent<Animator> (); 
@@ -15,12 +19,13 @@ public class LookAtController : MonoBehaviour {
   }
 
   void Update () {
-    timeElapsed += Time.deltaTime;
+    // Updateごとの時間加算
+    timeElapsed += Time.deltaTime; 
+    noChangeTime += Time.deltaTime;
     // Debug.Log(timeElapsed);
 
     if (timeElapsed >= 1) { // 一定時間経過で処理開始
-      // targetPos = new Vector3(20f, 20f, 50f);
-
+      // Debug.Log("Time passed");
       float [] coordinates = ReadCSV(); // CSVから読み込み
       
       if (coordinates != null) { // CSVが空じゃなければ処理
@@ -29,23 +34,42 @@ public class LookAtController : MonoBehaviour {
         float y = coordinates[1] / 480;
         // Debug.Log(x);
         // Debug.Log(y);
-        // Debug.Log("Time passed");
 
-        // x, y座標をUnityの座標に変換
-        float x_unity = 0.5f - x;
-        float y_unity = 1.0f - y;
-        x_unity *= 20f;
-        y_unity *= 20f;
-        Debug.Log(x_unity);
-        Debug.Log(y_unity);
+        // x, y座標を直前のものと比較, 時間の更新
+        if (x != last_x || y != last_y) {
+          isChange = true;
+        } else {
+          isChange = false;
+        }
+        if (isChange == true) {
+          noChangeTime = 0.0f;
+        }
 
-        // 向いてもらう
-        targetPos = new Vector3(x_unity, y_unity, 50f);
+        // 直前の座標を更新
+        last_x = x;
+        last_y = y;
+
+        if (noChangeTime >= 5) {
+          // 座標が変化していない時間が一定以上なら視線をカメラに向かせる
+          targetPos = Camera.main.transform.position;
+
+        } else {
+          // x, y座標をUnityの座標に変換
+          float x_unity = 0.5f - x;
+          float y_unity = 1.0f - y;
+          x_unity *= 20f;
+          y_unity *= 20f;
+          Debug.Log(x_unity);
+          Debug.Log(y_unity);
+
+          // 視線を座標の方向へ向いてもらう
+          targetPos = new Vector3(x_unity, y_unity, 50f);
+          //向いてもらったら変化してない時間をリセット
+          noChangeTime = 0.0f;
+        }
       }
-
-      timeElapsed = 0.0f;
+      timeElapsed = 0.0f; // 経過時間をリセット
     }
-    // targetPos = new Vector3(1.0f, 1.0f, 0.5f);
   }
 
   private void OnAnimatorIK(int layerIndex)
